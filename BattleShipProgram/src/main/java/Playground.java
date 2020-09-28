879f5a1399ae992a49b03d223e0d9cce0eec7e83
Th3Dennis <62747122+Th3Dennis@users.noreply.github.com>
2020-09-28 16:23:37 +0200

    Created a method named getCorrectCoordinateAndPoints to use in placeShips

--- BattleShipProgram/src/main/java/Playground.java
+++ BattleShipProgram/src/main/java/Playground.java
@@ -1,5 +1,8 @@
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Playground {
 
@@ -9,6 +12,7 @@ public class Playground {
     private HashMap<Point,Boat> shotsPlayer2;
     private HashMap<Point,Boat> sunkenShipsPlayer1;
     private HashMap<Point,Boat> sunkenShipsPlayer2;
+    public Map<Character, Integer> letterMap = new HashMap<>();
 
 
     public Playground(){
@@ -18,11 +22,71 @@ public class Playground {
     this.shotsPlayer2 = new HashMap<>();
     this.sunkenShipsPlayer1 = new HashMap<>();
     this.sunkenShipsPlayer2 = new HashMap<>();
+
+    }
+
+    {
+        letterMap.put('A', 1);
+        letterMap.put('B', 2);
+        letterMap.put('C', 3);
+        letterMap.put('D', 4);
+        letterMap.put('E', 5);
+        letterMap.put('F', 6);
+        letterMap.put('G', 7);
+        letterMap.put('H', 8);
+        letterMap.put('I', 9);
+        letterMap.put('J', 10);
     }
 
 
     public void placeShips(int id){
+    ArrayList<Point> pointArrayList = getCorrectCoordinatesAndPoints("Please type in the location of the 5 boat: ");
 
+    for (Point p : pointArrayList){
+        System.out.println(p.toString());
+    }
+
+    }
+
+    private ArrayList<Point> getCorrectCoordinatesAndPoints(String message){
+        boolean isRegexValid = false;
+        Scanner s = new Scanner(System.in);
+        ArrayList<Point> pointArrayList = new ArrayList<>();
+        String input = "";
+        Point p1 = null;
+        Point p2 = null;
+
+        do {
+            System.out.println(message);
+            input = s.nextLine();
+
+            if (input.matches("[a-hA-H][1-8]\\s[a-hA-H][1-8]")) {
+                isRegexValid = true;
+                input = input.toUpperCase();
+                String[] locationAndDestination = input.split(" ");
+
+                //Create two points with the entered Coordinates, converts the letters to numbers
+                p1 = new Point(letterMap.get(locationAndDestination[0].charAt(0)),Integer.parseInt(locationAndDestination[0].substring(1,2)) );
+                p2 = new Point(letterMap.get(locationAndDestination[1].charAt(0)), Integer.parseInt(locationAndDestination[1].substring(1,2)));
+
+                //End the game if the user entered resign
+            } else if (input.equals("resign")){
+                isRegexValid = true;
+            }
+        } while (!isRegexValid);
+
+        pointArrayList.add(p1);
+        pointArrayList.add(p2);
+
+
+        return pointArrayList;
+    }
+
+    private ArrayList<Point> getPointsBetween(Point p1, Point p2, int shipLenght){
+
+
+
+        return null;
     }
 
     public boolean shoot(Point p){
