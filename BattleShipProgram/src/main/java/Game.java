3b31deb58379532a1c60fa3415a7a9c9029ea44e
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2020-10-19 13:21:14 +0200

    Created Player

--- BattleShipProgram/src/main/java/Game.java
+++ BattleShipProgram/src/main/java/Game.java
@@ -1,11 +1,13 @@
 public class Game {
 
     public void playGame(){
+        Playground playground = new Playground();
 
     }
 
     public void createUser(){
-
+    Player player1 = new Player("Nikolai");
+    Player player2 = new Player("Dennis");
     }
 
 }
