f2a89e20d00c4d47b1ac0cb3167aec42a82eb9d2
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2020-10-19 13:22:00 +0200

    Update Starter.java

--- BattleShipProgram/src/main/java/Starter.java
+++ BattleShipProgram/src/main/java/Starter.java
@@ -3,9 +3,7 @@ public class Starter {
     //Author:   Dennis & Nikolai
     //Start:    28.09.2020
     public static void main(String[] args) {
-
-    Playground playground = new Playground();
-    
+           
 
     }
 }
