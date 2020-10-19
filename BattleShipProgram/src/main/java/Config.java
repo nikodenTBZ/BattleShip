53eebfb39a4a244faf62f147e3668146bfed0199
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2020-10-19 14:54:42 +0200

    Created String Config

---
+++ BattleShipProgram/src/main/java/Config.java
@@ -0,0 +1,9 @@
+public class Config {
+
+    public static String line = "├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤";
+    public static String border = "│";
+    public static String letters = "\t\tA\t\tB\t\tC\t\tD\t\tE\t\tF\t\tG\t\tH\t\tI\t\tJ";
+    public static String tab = "\t";
+
+}
+
